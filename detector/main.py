0eb1cdade4e5255d421e57bf24ea8ce0c4d37feb
Arpita Dutta <42901276+arpitadu@users.noreply.github.com>
2020-11-09 00:16:00 +1300

    Update main.py

--- detector/main.py
+++ detector/main.py
@@ -58,9 +58,9 @@ parser.add_argument('--n_test', default=4, type=int, metavar='N',
 def main():
     global args
     args = parser.parse_args()
-    #config_training = import_module(args.config)
-    global config
-    config_training = config
+    config_training = import_module(args.config)
+    
+    config_training = import_module(args.config)
     # from config_training import config as config_training
     torch.manual_seed(0)
     torch.cuda.set_device(0)
